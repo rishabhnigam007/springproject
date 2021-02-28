9160f7484805d80384df24d0c1163eb0bf37b70f
Rishabh Nigam <65018070+rishabhnigam007@users.noreply.github.com>
2021-02-28 15:38:41 +0530

    Add files via upload
    Hibernate Mapping Many-to-One

---
+++ Spring/MappingHibernate2/src/main/java/hibernate/mappings/App.java
@@ -0,0 +1,47 @@
+package hibernate.mappings;
+
+import java.util.Scanner;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		Configuration con = new Configuration().configure().addAnnotatedClass(Student.class)
+				.addAnnotatedClass(Laptop.class);
+		SessionFactory sf = con.buildSessionFactory();
+		Session s = sf.openSession();
+
+		Laptop laptop = new Laptop();
+		System.out.println("enter laptop id :");
+		int lid = sc.nextInt();
+		laptop.setLid(lid);
+		System.out.println("enter laptop name :");
+		String lname = sc.next();
+		laptop.setLname(lname);
+		
+		Student student = new Student();
+		System.out.println("enter student rollno :");
+		int srollno = sc.nextInt();
+		student.setRollno(srollno);
+		System.out.println("enter student marks :");
+		int smarks = sc.nextInt();
+		student.setMarks(smarks);
+		System.out.println("enter student name :");
+		String sname = sc.next();
+		student.setName(sname);
+		
+		//for many laptop to one student mapping so in student we assign one to many mapping 
+		student.getLaptop().add(laptop);
+		
+		s.beginTransaction();
+		s.save(laptop);
+		s.save(student);
+		System.out.println(student);
+		s.getTransaction().commit();
+		sc.close();
+	}
+}
---
+++ Spring/MappingHibernate2/src/main/java/hibernate/mappings/Laptop.java
@@ -0,0 +1,43 @@
+package hibernate.mappings;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "laptop")
+public class Laptop {
+	@Id
+	private int lid;
+	private String lname;
+	
+	@ManyToOne
+	private Student student;
+
+	public int getLid() {
+		return lid;
+	}
+
+	public void setLid(int lid) {
+		this.lid = lid;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+
+}
