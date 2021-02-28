fa034e6ad076ea454fa5aaa7d4711fd7e0d7ee89
Rishabh Nigam <65018070+rishabhnigam007@users.noreply.github.com>
2021-02-28 15:39:32 +0530

    Add files via upload
    Hibernate Mapping Many-to-Many

---
+++ Spring/MappingHibernate3/src/main/java/hibernate/mappings/Student.java
@@ -0,0 +1,60 @@
+package hibernate.mappings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+	@Id
+	private int rollno;
+	private int marks;
+	private String name;
+
+	@ManyToMany(mappedBy = "student")
+	private List<Laptop> laptop = new ArrayList<Laptop>();
+
+	public List<Laptop> getLaptop() {
+		return laptop;
+	}
+
+	public void setLaptop(List<Laptop> laptop) {
+		this.laptop = laptop;
+	}
+
+	public int getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+	public int getMarks() {
+		return marks;
+	}
+
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [rollno=" + rollno + ", marks=" + marks + ", name=" + name + ", laptop=" + laptop + "]";
+	}
+
+	
+}
